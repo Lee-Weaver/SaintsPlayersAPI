05de4b4e3243d216e956f8d7ad435b4b0427ce93
Lee-Weaver <leeweaver23@gmail.com>
2023-04-02 16:30:36 +0100

    SaintsPlayer list is now ordered by most appearances to least

--- SaintsPlayersAPI/Controllers/SaintsPlayersController.cs
+++ SaintsPlayersAPI/Controllers/SaintsPlayersController.cs
@@ -30,6 +30,8 @@ namespace SaintsPlayersAPI.Controllers
           {
               return NotFound();
           }
+            _context.SaintsPlayer.OrderByDescending(e
+                  => e.Appearances);
             return await _context.SaintsPlayer.ToListAsync();
         }
 
