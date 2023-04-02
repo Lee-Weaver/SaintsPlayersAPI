ae683e943aa8d022e77e28af7fc9efaff8aeb2c3
Lee-Weaver <leeweaver23@gmail.com>
2023-04-02 22:07:36 +0100

    Created js frontend which calls api and creates table

---
+++ SaintsPlayersAPI/wwwroot/js/site.js
@@ -0,0 +1,28 @@
+﻿const uri = "api/saintsplayers";
+let saintsPlayers = [];
+function getItems() {
+    fetch(uri)
+        .then(response => response.json())
+        .then(data => _displayItems(data))
+        .catch(error => console.error("Unable to get items.", error));
+}
+
+function _displayItems(data) {
+    console.log(data)
+    const tBody = document.getElementById('saintsTable');
+    tBody.innerHTML = '';
+    data.forEach(item => {
+        let tr = tBody.insertRow();
+        insertRow(item.name, 0, tBody, tr);
+        insertRow(item.position, 1, tBody, tr);
+        insertRow(item.country, 2, tBody, tr);
+        insertRow(item.appearances, 3, tBody, tr);
+        insertRow(item.goals, 4, tBody, tr);
+    })
+}
+
+function insertRow(string, position, tBody, tr) {
+    let td = tr.insertCell(position);
+    let textNode = document.createTextNode(string);
+    td.appendChild(textNode);
+}
